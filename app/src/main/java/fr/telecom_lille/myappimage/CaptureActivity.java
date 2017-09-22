b8916b09b1bdf32fb6d1e2ae9e51858c6dffb98c
FrancoisTelecom <francois.5@hotmail.fr>
2017-09-22 11:28:59 +0200

    Impl appPhoto + insert de la photo fonctionnel

--- app/src/main/java/fr/telecom_lille/myappimage/CaptureActivity.java
+++ app/src/main/java/fr/telecom_lille/myappimage/CaptureActivity.java
@@ -5,7 +5,9 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +16,9 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by ZMNF8866 on 10/09/2017.
@@ -24,7 +29,9 @@ public class CaptureActivity extends AppCompatActivity implements View.OnClickLi
     private Button takePhoto;
     static final int REQUEST_IMAGE_CAPTURE = 1;
     ImageView mImageView;
-    File mCurrentPhotoPath;
+    String mCurrentPhotoPath;
+
+
 
 
 
@@ -33,6 +40,8 @@ public class CaptureActivity extends AppCompatActivity implements View.OnClickLi
         setContentView(R.layout.activity_capture);
         createButton(Breturn,R.id.Creturn);
         createButton(takePhoto,R.id.TakePhoto);
+        mImageView = (ImageView) findViewById(R.id.imgPhoto);
+        //Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
     }
 
     public void onClick(View v) {
@@ -45,7 +54,12 @@ public class CaptureActivity extends AppCompatActivity implements View.OnClickLi
                 break;
             case R.id.TakePhoto:
                 dispatchTakePictureIntent();
-
+               /* try {
+                    createImageFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }*/
+                galleryAddPic();
         }
 
     }
@@ -66,14 +80,51 @@ public class CaptureActivity extends AppCompatActivity implements View.OnClickLi
         }
     }
 
-        private void galleryAddPic() {
-            Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-            File f = new File(String.valueOf(mCurrentPhotoPath));
-            Uri contentUri = Uri.fromFile(f);
-            mediaScanIntent.setData(contentUri);
-            this.sendBroadcast(mediaScanIntent);
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(String.valueOf(mCurrentPhotoPath));
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
         }
 
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    /*private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                        }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }*/
 
     private boolean createButton(Button but, int R){
         but = (Button)findViewById(R);
